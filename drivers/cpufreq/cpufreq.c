edaf4697d5c19312d4dabf3a07c98be1a37d4e31
Your Name <manveru0@gmail.com>
2012-09-29 01:09:44 +0200

    Add uV function Don't know who takes the credits for

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -551,6 +551,11 @@ static ssize_t show_scaling_setspeed(struct cpufreq_policy *policy, char *buf)
 		return sprintf(buf, "<unsupported>\n");
 
 	return policy->governor->show_setspeed(policy, buf);
+	
+	/* sysfs interface for UV control */
+	extern ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf);
+	extern ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
+                                      const char *buf, size_t count);
 }
 
 /**
@@ -582,6 +587,8 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
+/* UV table */
+cpufreq_freq_attr_rw(UV_mV_table);
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -595,6 +602,7 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
+	&UV_mV_table.attr,
 	NULL
 };
 
