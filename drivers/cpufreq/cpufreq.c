0c77df825ff68b5dd38f01b5c8a1d2165ac9608c
Your Name <manveru0@gmail.com>
2012-09-29 02:11:21 +0200

    Revert "Add uV function"
    This reverts commit edaf4697d5c19312d4dabf3a07c98be1a37d4e31.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -551,11 +551,6 @@ static ssize_t show_scaling_setspeed(struct cpufreq_policy *policy, char *buf)
 		return sprintf(buf, "<unsupported>\n");
 
 	return policy->governor->show_setspeed(policy, buf);
-	
-	/* sysfs interface for UV control */
-	extern ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf);
-	extern ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
-                                      const char *buf, size_t count);
 }
 
 /**
@@ -587,8 +582,6 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
-/* UV table */
-cpufreq_freq_attr_rw(UV_mV_table);
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -602,7 +595,6 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
-	&UV_mV_table.attr,
 	NULL
 };
 
